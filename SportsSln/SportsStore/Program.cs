c431f7508ee02d84d30f5650c3daa9670655b8b0
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-14 18:07:23 +0000

    General info added to Startup file.

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,13 +1,39 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+/*
+ * Set up the objects (a.k.a. services) to be used throughout
+ * the application, which are accessed through dependency injection.
+ * 
+ * This specific call sets up the shared objects required by applications
+ * using MVC framework and Razor view engine.
+ */
 builder.Services.AddControllersWithViews();
 
+/*
+ * Display details of the exceptions which occur in the application.
+ * Useful furing the development process, but not to be enabled in
+ * deployed applications.
+ */
 app.UseDeveloperExceptionPage();
-app.UseStatusCodePages();
-app.UseStaticFiles();
-app.UseRouting();
 
+/*
+ * Adds a simple message to HTTP reponses which would otherwise
+ * have no body.
+ */
+app.UseStatusCodePages();
+
+/*
+ * Enables support for serving static content from the wwwroot folder.
+ */
+app.UseStaticFiles();
+
+/*
+ * These two methods add the endpoint routing feature to the request
+ * pipeline, which is what matches HTTP requests to to the application
+ * features (i.e. endpoints) able to produce responses for them.
+ */
+app.UseRouting();
 app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
 
 app.Run();
