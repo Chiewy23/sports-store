4700bfb7b8ab49cc53405486edce3917c693ff4e
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-15 19:17:22 +0000

    Implemented instance of IStoreRepository.

---
+++ SportsSln/SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsStore.Models {
+	public class EFStoreRepository : IStoreRepository {
+
+		private readonly StoreDbContext context;
+
+		public EFStoreRepository(StoreDbContext context) {
+			this.context = context;
+		}
+
+		public IQueryable<Product> Products => this.context.Products;
+	}
+}
