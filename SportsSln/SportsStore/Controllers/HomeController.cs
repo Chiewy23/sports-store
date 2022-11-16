5ba3d1605c03af7c1a4d7a30dd1b5b41ed45cc65
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-16 15:34:01 +0000

    Repository instance created in HomeController.

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,7 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers {
 	public class HomeController : Controller {
-		public IActionResult Index() => View();
+		private readonly IStoreRepository repository;
+
+		public HomeController(IStoreRepository repository) {
+			this.repository = repository;
+		}
+
+		public IActionResult Index() => View(repository.Products);
 	}
 }
