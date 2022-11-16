7a6258044702b8b6db30b84a69a02cc21e392f76
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-16 15:39:43 +0000

    Comments added for DI process.

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -5,6 +5,18 @@ namespace SportsStore.Controllers {
 	public class HomeController : Controller {
 		private readonly IStoreRepository repository;
 
+		/**
+		 * When ASP.NET Core creates a new instance of the HomeController
+		 * class to handle a HTTP request, it inspects the contructor and
+		 * sees it requires an object which implements the IStoreRepository
+		 * interface.
+		 * 
+		 * To determine which implementation class should be used, ASP.NET
+		 * Core consults the configuration in the Program class which (in this case)
+		 * tells it to create a new instance of EFStoreRepository for every request.
+		 * 
+		 * This process is known as dependency injection.
+		 */
 		public HomeController(IStoreRepository repository) {
 			this.repository = repository;
 		}
