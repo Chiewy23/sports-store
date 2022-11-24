c8a81b76e605372e55d61a46df9f57d5237edd9c
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-24 15:41:20 +0000

    Using RouteData to access url properties.

--- SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -10,7 +10,20 @@ namespace SportsStore.Components {
 			this.repository = repository;
 		}
 
+		/**
+		 * The ViewComponent base class provides access to context objects through
+		 * a set of properties, one of which is RouteData. This provides info about
+		 * how the request URL was handled by the routing system.
+		 * 
+		 * We can use this to get the value of the currently selected category
+		 * and pass it to the view.
+		 * 
+		 * In most cases we would do so by creating another view model class, but 
+		 * we can also use the view bag feature, which allows unstructured data to 
+		 * be passed to a view alongside the view model object.
+		 */
 		public IViewComponentResult Invoke() {
+			ViewBag.SelectedCategory = RouteData?.Values["category"];
 			return View(repository.Products
 				.Select(p => p.Category)
 				.Distinct()
