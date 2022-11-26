948a847bda243c5c58bef74c9b6e8cfb8726f390
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-26 11:55:36 +0000

    URL extensions class created.

---
+++ SportsSln/SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+namespace SportsStore.Infrastructure {
+	public static class UrlExtensions {
+
+		/* Generate the URL the browser will be returned to after the cart has been
+		 * updated, taking into account the query string (if there is one).
+		 */
+		public static string PathAndQuery(this HttpRequest request) {
+			return request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
+		}
+	}
+}
