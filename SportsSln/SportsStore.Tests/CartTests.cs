98d0a9e063e889780d9f56e97fa913f0779ab3db
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-28 15:22:05 +0000

    First unit test or cart.

---
+++ SportsSln/SportsStore.Tests/CartTests.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Models;
+
+namespace SportsStore.Tests {
+	public class CartTests {
+
+		[Fact]
+		public void CanAddNewLines() {
+			// Arrange
+			var p1 = new Product { ProductID = 1, Name = "P1" };
+			var p2 = new Product { ProductID = 2, Name = "P2" };
+
+			var target = new Cart();
+
+			// Act
+			target.AddItem(p1, 1);
+			target.AddItem(p2, 1);
+			var results = target.Lines.ToArray();
+
+			// Assert
+			Assert.Equal(2, results.Length);
+			Assert.Equal(p1, results[0].Product);
+			Assert.Equal(p2, results[1].Product);
+		}
+	}
+}
