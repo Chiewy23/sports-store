babc7558aead405aec294925f0bc21ab65bae898
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-28 15:12:48 +0000

    New Cart model.

---
+++ SportsSln/SportsStore/Models/Cart.cs
@@ -0,0 +1,24 @@
+﻿namespace SportsStore.Models {
+	public class Cart {
+		public List<CartLine> Lines { get; set; } = new List<CartLine>();
+
+		public void AddItem(Product product, int quantity) {
+			CartLine line = Lines.FirstOrDefault(x => x.Product.ProductID == product.ProductID);
+
+			if (line == null) {
+				Lines.Add(new CartLine {
+					Product = product,
+					Quantity = quantity
+				});
+			} else {
+				line.Quantity += quantity;
+			}
+		}
+	}
+
+	public class CartLine {
+		public int CartLineID { get; set; }
+		public Product Product { get; set; }
+		public int Quantity { get; set; }
+	}
+}
