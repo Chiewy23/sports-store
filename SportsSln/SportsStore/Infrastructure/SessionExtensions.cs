65ce7caf4c3e3382f2c2060496b2c58d7e94da05
Christopher Hiew <christopherhiew@protonmail.com>
2022-11-29 18:07:00 +0000

    First extension method ISession.

--- SportsSln/SportsStore/Infrastructure/SessionExtensions.cs
+++ SportsSln/SportsStore/Infrastructure/SessionExtensions.cs
@@ -1,4 +1,21 @@
-﻿namespace SportsStore.Infrastructure {
-	public class SessionExtensions {
+﻿using System.Text.Json;
+
+namespace SportsStore.Infrastructure {
+
+	/*
+	 * The session state feature in ASP.NET Core only stores int, string, and byte[]
+	 * values. We need to store a Cart object, so will define extension methods to the
+	 * ISession interface, which provide access to the session state data to serialise
+	 * Cart objects into JSON and convert them back.
+	 */
+	public static class SessionExtensions {
+		public static void SetJson(this ISession session, string key, object value) {
+			session.SetString(key, JsonSerializer.Serialize(value));
+		}
+
+		public static T? GetJson<T>(this ISession session, string key) {
+			var sessionData = session.GetString(key);
+			return sessionData == null ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
+		}
 	}
 }
