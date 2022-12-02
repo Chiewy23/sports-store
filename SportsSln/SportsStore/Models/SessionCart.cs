940490f4727ed43c3f596c877b9653e9a1e78d41
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-02 19:24:15 +0000

    Making prop nullable.

--- SportsSln/SportsStore/Models/SessionCart.cs
+++ SportsSln/SportsStore/Models/SessionCart.cs
@@ -13,21 +13,21 @@ namespace SportsStore.Models {
 		}
 
 		[JsonIgnore]
-		public ISession Session { get; set; }
+		public ISession? Session { get; set; }
 
 		public override void AddItem(Product product, int quantity) {
 			base.AddItem(product, quantity);
-			Session.SetJson("Cart", this);
+			Session?.SetJson("Cart", this);
 		}
 
 		public override void RemoveLine(Product product) {
 			base.RemoveLine(product);
-			Session.SetJson("Cart", this);
+			Session?.SetJson("Cart", this);
 		}
 
 		public override void Clear() {
 			base.Clear();
-			Session.Remove("Cart");
+			Session?.Remove("Cart");
 		}
 	}
 }
