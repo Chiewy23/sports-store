81108e913347ed73ca313183ebfd1ba45b596173
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-08 14:50:33 +0000

    Updating OrderController.

--- SportsSln/SportsStore/Controllers/OrderController.cs
+++ SportsSln/SportsStore/Controllers/OrderController.cs
@@ -3,8 +3,34 @@ using SportsStore.Models;
 
 namespace SportsStore.Controllers {
 	public class OrderController : Controller {
+
+		private readonly IOrderRepository repository;
+		private readonly Cart cart;
+
+		public OrderController(IOrderRepository repository, Cart cart) {
+			this.repository = repository;
+			this.cart = cart;
+		}
+
 		public IActionResult Checkout() {
 			return View(new Order());
+		}
+
+		[HttpPost]
+		public IActionResult Checkout(Order order) {
+			if (cart.Lines.Count() == 0) {
+				ModelState.AddModelError("", "Sorry, your cart is empty!");
+			}
+
+			if (ModelState.IsValid) {
+				order.Lines = cart.Lines;
+				repository.SaveOrder(order);
+				cart.Clear();
+
+				return RedirectToPage("/Completed", new { orderId = order.OrderID });
+			} else {
+				return View();
+			}
 		}
 	}
 }
