9b37c6ae86163428156279851a257a0964a1be78
Christopher Hiew <christopherhiew@protonmail.com>
2022-12-24 18:18:56 +0000

    Temp admin user created.

---
+++ SportsSln/SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models {
+	public class IdentitySeedData {
+		private const string adminUser = "Admin";
+		private const string adminPassword = "Secret123$";
+
+		public static async void EnsurePopulated(IApplicationBuilder app) {
+			var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+			if (context.Database.GetPendingMigrations().Any()) {
+				context.Database.Migrate();
+			}
+
+			var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+			var user = await userManager.FindByIdAsync(adminUser);
+
+			if (user == null) {
+				user = new IdentityUser("Admin");
+				user.Email= "admin@example.com";
+				user.PhoneNumber = "555-1234";
+
+				await userManager.CreateAsync(user, adminPassword);
+			}
+		}
+	}
+}
